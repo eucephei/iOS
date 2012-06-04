7ab802c613a70f0f6df6e7fcc2d50079cb664ce1
eucephei <eucephei@gmail.com>
2012-06-04 10:51:04 +0800

    Initial Commit

---
+++ FlickrPlaces/FlickrPlaces/FlickrPlacesAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FlickrPlacesAppDelegate.h
+//  FlickrPlaces
+//
+//  Created by ace on 1/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FlickrPlacesAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FlickrPlaces/FlickrPlaces/FlickrPlacesViewController.h
@@ -0,0 +1,13 @@
+//
+//  FlickrPlacesViewController.h
+//  FlickrPlaces
+//
+//  Created by ace on 1/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FlickrPlacesViewController : UIViewController
+
+@end
